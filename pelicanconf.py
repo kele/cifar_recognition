a6f5e1b2a058389d318d5ada4ad0bd0809d457e3
kele <damian.bogel@gmail.com>
2016-01-18 19:16:51 +0100

    new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Warsaw'
 
 DEFAULT_LANG = 'en'
 
+THEME='./clean-theme'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
