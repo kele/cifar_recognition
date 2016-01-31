453e7f7179313bcc717b9291065dad855ba2eddd
kele <damian.bogel@gmail.com>
2016-01-31 13:15:31 +0100

    It is AFAIR the best network I have.

--- cifar/network.py
+++ cifar/network.py
@@ -8,290 +8,62 @@ from lasagne.nonlinearities import *
 
 from network_building.network_building import build_network
 
-def build_cnn_network75(input_variable, batch_size=None):
-    layers = [
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 256,
-             'nonlinearity': rectify}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
-
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
-
-
-def build_cnn_network75extended(input_variable, batch_size=None):
-    layers = [
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 64,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify,
-             'pad': 1}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify,
-             'pad': 1}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 256,
-             'nonlinearity': rectify}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 128,
-             'nonlinearity': rectify}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
-
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
 
 # elu
 def elu(x):
     return theano.tensor.switch(x > 0, x, theano.tensor.exp(x) - 1)
 
-def build_cnn_network_from_paper(input_variable, batch_size=None):
-    layers = [
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
 
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 96,
-             'filter_size': (5, 5),
-             'nonlinearity': elu,
-             'pad': 1}},
+best_network = [
+    {'type': DropoutLayer,
+        'args': {'p': 0.25}},
 
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (3, 3), 'stride': 2}},
+    {'type': Conv2DLayer,
+        'args': {
+            'num_filters': 96,
+            'filter_size': (5, 5),
+            'nonlinearity': elu,
+            'pad': 1}},
 
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
+    {'type': Conv2DLayer,
+        'args': {
+            'num_filters': 64,
+            'filter_size': (5, 5),
+            'nonlinearity': elu,
+            'pad': 1}},
 
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 192,
-             'filter_size': (5, 5),
-             'nonlinearity': elu,
-             'pad': 1}},
+    {'type': MaxPool2DLayer,
+        'args': {'pool_size': (2, 2)}},
 
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (3, 3), 'stride': 2}},
+    {'type': DropoutLayer,
+        'args': {'p': 0.5}},
 
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
+    {'type': Conv2DLayer,
+        'args': {
+            'num_filters': 64,
+            'filter_size': (5, 5),
+            'nonlinearity': elu,
+            'pad': 1}},
 
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 192,
-             'filter_size': (3, 3),
-             'nonlinearity': elu}},
+    {'type': MaxPool2DLayer,
+        'args': {'pool_size': (3, 3), 'stride': 2}},
 
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 192,
-             'filter_size': (1, 1),
-             'nonlinearity': elu}},
+    {'type': DropoutLayer,
+        'args': {'p': 0.5}},
 
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
+    {'type': FlattenLayer,
+        'args': {'outdim': 2}},
 
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
+    {'type': DenseLayer,
+        'args': {
+            'num_units': 256,
+            'nonlinearity': elu}},
 
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
+    {'type': DropoutLayer,
+        'args': {'p': 0.5}},
 
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
-
-
-def build_cnn_network(input_variable, batch_size=None):
-    layers = [
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 256,
-             'nonlinearity': rectify}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
-
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
-
-
-def build_cnn_network2(input_variable, batch_size=None):
-    layers = [
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 64,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 64,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 512,
-             'nonlinearity': rectify}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
-
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
-
-
-def build_cnn_network3(input_variable, batch_size=None):
-    layers = [
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 64,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 64,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.25}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': Conv2DLayer,
-         'args': {
-             'num_filters': 32,
-             'filter_size': (5, 5),
-             'nonlinearity': rectify}},
-
-        {'type': MaxPool2DLayer,
-         'args': {'pool_size': (2, 2)}},
-
-        {'type': DropoutLayer,
-         'args': {'p': 0.5}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 512,
-             'nonlinearity': rectify}},
-
-        {'type': DenseLayer,
-         'args': {
-             'num_units': 10,
-             'nonlinearity': softmax}},
-    ]
-
-    return build_network(layers, input_variable, (batch_size,) + DATA_SHAPE), layers
+    {'type': DenseLayer,
+        'args': {
+            'num_units': 10,
+            'nonlinearity': softmax}},
+]
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 
-import engine
-import theano.tensor as T
-import lasagne
+import cifar.network
 import datetime
+import engine
+import lasagne
+import pickle
+import theano.tensor as T
 import time
 
-import pickle
+
+BATCH_SIZE = 100
+
 
 def save_results(layers, params, filename, training_schedule):
     pickle.dump((layers, params, training_schedule), open(filename, 'w'))
 
 
+def load_network(filename):
+    layers, prm, _ = pickle.load(open(filename))
+
+    network, _ = cifar.network.build_network(best_network, batch_size=BATCH_SIZE)
+    lasagne.layers.set_all_param_values(network, prm)
+    return network, layers
+
+
 def main():
     import cifar.network
-    BATCH_SIZE = 100
 
     full_datastream = cifar.data.load_datastream(BATCH_SIZE)
     small_datastream = cifar.data.load_datastream(BATCH_SIZE,
@@ -28,94 +39,89 @@ def main():
     targets_var = T.ivector('targets')
 
     print('Building the network...')
-    network, layers = cifar.network.build_cnn_network_from_paper(input_var, batch_size=BATCH_SIZE)
-
-    #print('Loading old network')
-    #_, prm, _ = pickle.load(open('saved_nets/partial_49.30accuracy_2016-01-26__01-18-01.params'))
-    #lasagne.layers.set_all_param_values(network, prm)
-    #print('Network loaded!')
+    network, layers = cifar.network.build_network(cifar.network.best_network,
+                                                  batch_size=BATCH_SIZE)
 
     print('Network:')
     for l in layers:
         print '  ', l['type'], '\n    ', l['args']
 
-    for basic_lr in [0.01, 0.005, 0.001]:
-        print('Learning schedule:')
-        schedule = [
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
-            {'num_epochs': 20, # tutaj jeszcze przed chwila bylo 5
-            'hyperparams': {
-                'learning_rate': basic_lr / 2,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr / 10,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr / 10 / 2,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr / 100,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
+    basic_lr = 0.01
+    schedule = [
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
+        {'num_epochs': 20, # tutaj jeszcze przed chwila bylo 5
+        'hyperparams': {
+            'learning_rate': basic_lr / 2,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr / 10,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr / 10 / 2,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr / 100,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
 
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr / 100 / 2,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr / 100 / 2,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
 
-            {'num_epochs': 20,
-            'hyperparams': {
-                'learning_rate': basic_lr / 1000,
-                'weight_decay': 0.001,
-                'momentum': 0.9,
-                }},
-        ]
+        {'num_epochs': 20,
+        'hyperparams': {
+            'learning_rate': basic_lr / 1000,
+            'weight_decay': 0.001,
+            'momentum': 0.9,
+            }},
+    ]
 
-        for s in schedule:
-            print '  ', s
+    for s in schedule:
+        print '  ', s
 
-        print('Starting training...')
+    print('Starting training...')
 
-        schedule_passed = []
-        for s in schedule:
-            print 'Current schedule: ', s
-            _, test_acc = \
-                engine.train(
-                    input_var=input_var,
-                    targets_var=targets_var,
-                    data=full_datastream,
-                    network=network,
-                    verbose=1,
-                    patience=5,
-                    **s)
+    schedule_passed = []
+    for s in schedule:
+        print 'Current schedule: ', s
+        _, test_acc = \
+            engine.train(
+                input_var=input_var,
+                targets_var=targets_var,
+                data=full_datastream,
+                network=network,
+                verbose=1,
+                patience=5,
+                **s)
 
-            print('Saving results in 5 seconds...')
-            time.sleep(5)
-            schedule_passed += s
+        print('Saving results in 5 seconds...')
+        time.sleep(5)
+        schedule_passed += s
 
-            t = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d__%H-%M-%S')
-            save_results(layers, params=lasagne.layers.get_all_param_values(network),
-                        training_schedule=schedule_passed,
-                        filename='saved_nets/partial_{:.2f}accuracy_lr{}_{}.params'.format(
-                            test_acc, s['hyperparams']['learning_rate'], t))
-        print('Training finished')
+        t = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d__%H-%M-%S')
+        save_results(layers, params=lasagne.layers.get_all_param_values(network),
+                    training_schedule=schedule_passed,
+                    filename='saved_nets/partial_{:.2f}accuracy_lr{}_{}.params'.format(
+                        test_acc, s['hyperparams']['learning_rate'], t))
+    print('Training finished')
 
 if __name__ == '__main__':
     main()
